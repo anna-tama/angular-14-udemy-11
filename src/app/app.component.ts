88c39b849f6ff8d2f9ea40bf1a5c0adc4824aceb
anna_tama <anna.tama.roma@gmail.com>
2022-08-28 17:23:34 -0300

    basic: comunicacion entre componentes

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //template:'<span>Tamara</span>', //Reemplaza aquello que esté en el componente por lo que escribo acá
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+ }
+
+
